b3ec976e4b2aa1b4c15249d815a2c6dddd1a9fc9
Lera <lerizlot@icloud.com>
2022-12-15 13:32:01 +0400

    added Lesson1

---
+++ Lesson1/Ex005_ConditionalIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hooray, It's Masha !!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
