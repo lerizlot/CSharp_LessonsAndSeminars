8e7d6acd7aa10ecf49e2a2f22aaafa1c0fd9f7ee
Lera <lerizlot@icloud.com>
2022-12-15 13:32:30 +0400

    added Seminar1

---
+++ Seminar1/Examples/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = 5;
+double b = 3.14;
+bool c = true; // false
+string d = "строка";
+
+// два варианта конкатенации
+Console.WriteLine("My number is " + a);
+Console.WriteLine($"My number is {a}");
+
+// два вида вывода в консоль
+Console.Write("Good morning! ");
+Console.WriteLine("Have a nice day! ");
+
+Console.WriteLine("Input a number: ");
+// преобразование типа
+int number = Convert.ToInt32(Console.ReadLine());
+
+// остаток от деления %
+
+/*
+37145 / 10 = 3714
+37145 / 100 = 371
+37145 / 1000 = 37
+37145 / 10000 = 3
+37145 / 100000 = 0
+
+37145 % 10 = 5
+37145 % 100 = 45
+37145 % 1000 = 145
+37145 % 10000 = 7145
+*/
---
+++ Seminar1/Tasks/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b * b == a)
+    Console.WriteLine("Число a является квадратом числа b");
+else
+    Console.WriteLine("Число a не является квадратом числа b");
+
+// Задача 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = -N;
+
+while (current <= N)
+{
+    Console.Write(current + " ");
+    current++;
+}
+Console.WriteLine();
+
+// альтернативный вариант решения через цикл for
+for (int i = -N; i <= N; i++)
+    Console.Write(i + " ");
