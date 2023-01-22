63263cafaa97564790170a721bc4a2574ca37e57
Lera <lerizlot@icloud.com>
2023-01-22 23:36:32 +0400

    added Seminar5

---
+++ Seminar5/Examples/Program.cs
@@ -0,0 +1,63 @@
+﻿int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+/*
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input an {i + 1} element: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+*/
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных элементов массива.
+
+int GetSumOfNegative(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] < 0)
+            sum += array[i];
+    return sum;
+}
+
+Console.Write("Input a number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(length, min, max);
+ShowArray(newArray);
+
+int result = GetSumOfNegative(newArray);
+Console.WriteLine($"The sum of negative elements is: {result}");
+
+/*
+несколько типов методов по работе с массивами
+1. генерирующие методы - создают массив
+2. аналитические методы - используют массив как первоисточник для расчетов но не видоизменяют его
+3. преобразующие методы - берут на вход массив но при этом возвращают преобразованную версию исходного массива
+*/
---
+++ Seminar5/Tasks/Program.cs
@@ -0,0 +1,91 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+int[] GetReverse(int[] firstArray)
+{
+    for (int i = 0; i < firstArray.Length; i++) 
+        firstArray[i] = firstArray[i] * -1;
+    return firstArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("Task1: ");
+
+Console.Write("Input a number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] firstArray = CreateRandomArray(length, min, max);
+ShowArray(firstArray);
+
+int[] result1 = GetReverse(firstArray);
+ShowArray(result1);
+Console.WriteLine("-----------------");
+
+// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+
+bool GetSearch(int[] array, int x)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == x) return true;
+    }
+    return false;
+}
+Console.WriteLine("Task2: ");
+
+Console.WriteLine("Input a number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int[] secondArray = CreateRandomArray(length, min, max);
+ShowArray(secondArray);
+
+Console.WriteLine(GetSearch(secondArray, x));
+Console.WriteLine("-----------------");
+
+
+// Задайте одномерный массив из m случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [a,b]
+
+int GetQuantity(int[] array, int minNumb, int maxNumb)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] >= minNumb && array[i] <= maxNumb)
+        {
+            count += 1;
+            // Console.WriteLine(array[i]);
+        }
+    return count;
+}
+
+Console.WriteLine("Task3: ");
+
+Console.WriteLine("Input a dot a: ");
+int minNumb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a dot b: ");
+int maxNumb = Convert.ToInt32(Console.ReadLine());
+
+int[] thirdArray = CreateRandomArray(length, min, max);
+ShowArray(thirdArray);
+
+int result3 = GetQuantity(thirdArray, minNumb, maxNumb);
+Console.WriteLine($"The quantity of elements between {minNumb} and {maxNumb} is - {result3}");
