5042b601a9a5253754d753cded2bf4db3d3980ee
Lera <lerizlot@icloud.com>
2023-02-01 19:57:10 +0400

    added Seminar9

---
+++ Seminar9/Examples/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+void ShowNums(int num)
+{
+    if (num > 1) ShowNums(num - 1);
+    Console.Write(num + " ");
+}
+
+ShowNums(5);
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int GetSumOfDigits(int num)
+{
+    if (num != 0) return GetSumOfDigits(num / 10) + num % 10;
+    else return 0;
+}
+Console.WriteLine(GetSumOfDigits(1234));
---
+++ Seminar9/Tasks/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void ShowNums(int M, int N)
+{
+    if(M > N)
+    {
+        Console.Write(M + " ");
+        ShowNums(M - 1, N);
+    }
+    else if (M < N) ShowNums(M, N - 1);
+    else Console.WriteLine(N + " ");
+}
+
+ShowNums(3,9);
+
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B.
+
+double Method(double A, double B)
+{
+    if(B>0) return A*Method(A, B -1);
+    else if (B < 0) return (1/A)*Method(A, B+1);
+    else return 1;
+}
+
+Console.WriteLine(Method(2, -3));
